5177808982139497cb74656a7982124bad1eac67
Murat Uenalan <murat.uenalan@gmail.com>
2023-05-20 12:22:07 +0200

    going public

--- platforms/darwin19/.configureplus/global.bash
+++ platforms/darwin19/.configureplus/global.bash
@@ -4,5 +4,6 @@
 export CONFIGUREPLUS_SESSION='darwin19'
 export CONFIGURE_DIR_OUTPUT='platforms'
 export CONFIGURE_DIR_TEMPLATE='template'
+export CONFIGURE_GIT_TAG='v0.0.1'
 export CONFIGURE_PKGNAME='configureplus'
-export CONFIGURE_VERSION='0.1'
+export CONFIGURE_VERSION='0.0.1'
