068012fdec594c98939014356c3993d915b019ee
Murat Uenalan <murat.uenalan@gmail.com>
2023-05-22 10:56:52 +0200

    cleanup

--- template/.configureplus/currentsession.bash
+++
@@ -1,3 +0,0 @@
-CONFIGUREPLUS_SESSION=$(cat .configureplus/global/CONFIGUREPLUS_SESSION)
-
-source .configureplus/session/$CONFIGUREPLUS_SESSION.bash
--- template/.configureplus/currentsession.mk
+++
@@ -1,3 +0,0 @@
-CONFIGUREPLUS_SESSION=$(shell cat .configureplus/global/CONFIGUREPLUS_SESSION)
-
-include .configureplus/session/$(CONFIGUREPLUS_SESSION).mk
