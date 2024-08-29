b75f59d8f3d61e7382e65e98d696614186b9f203
Murat Uenalan <murat.uenalan@gmail.com>
2024-08-29 14:26:21 +0200

    bugfix: overwriting session with global, *each time*. Now only if not existant.

--- build/platforms/linux-gnu/.configureplus/global.bash
+++ build/platforms/linux-gnu/.configureplus/global.bash
@@ -1,8 +1,9 @@
 
-# Generated with configureplus (CONFIGUREPLUS_VERSION=0.1, CONFIGUREPLUS_PWD=/home/muenalan/git-workdirs/github.com/muenalan/bash/configureplus/platforms/linux-gnu)
+# Generated with configureplus (CONFIGUREPLUS_VERSION=0.14.0, CONFIGUREPLUS_PWD=/home/muenalan/git-workdirs/configureplus/build/platforms/linux-gnu)
 
+export CONFIGUREPLUS_SESSION='linux-gnu'
 export CONFIGURE_DIR_OUTPUT='platforms'
 export CONFIGURE_DIR_TEMPLATE='template'
+export CONFIGURE_GIT_TAG='v0.0.1'
 export CONFIGURE_PKGNAME='configureplus'
-export CONFIGUREPLUS_SESSION='linux-gnu'
 export CONFIGURE_VERSION='0.0.1'
--- build/platforms/linux-gnu/.configureplus/global.mk
+++ build/platforms/linux-gnu/.configureplus/global.mk
@@ -1,9 +1,10 @@
 
-# Generated with configureplus (CONFIGUREPLUS_VERSION=0.1, CONFIGUREPLUS_PWD=/home/muenalan/git-workdirs/github.com/muenalan/bash/configureplus/platforms/linux-gnu)
+# Generated with configureplus (CONFIGUREPLUS_VERSION=0.14.0, CONFIGUREPLUS_PWD=/home/muenalan/git-workdirs/configureplus/build/platforms/linux-gnu)
 # Note: space before a comment are part of a variable
 
+CONFIGUREPLUS_SESSION=$(shell cat .configureplus/global/CONFIGUREPLUS_SESSION)
 CONFIGURE_DIR_OUTPUT=$(shell cat .configureplus/global/CONFIGURE_DIR_OUTPUT)
 CONFIGURE_DIR_TEMPLATE=$(shell cat .configureplus/global/CONFIGURE_DIR_TEMPLATE)
+CONFIGURE_GIT_TAG=$(shell cat .configureplus/global/CONFIGURE_GIT_TAG)
 CONFIGURE_PKGNAME=$(shell cat .configureplus/global/CONFIGURE_PKGNAME)
-CONFIGUREPLUS_SESSION=$(shell cat .configureplus/global/CONFIGUREPLUS_SESSION)
 CONFIGURE_VERSION=$(shell cat .configureplus/global/CONFIGURE_VERSION)
