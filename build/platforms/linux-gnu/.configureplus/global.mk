0f2907699cd6361bd00921d1f3cafaae3943bff2
Murat Uenalan <murat.uenalan@gmail.com>
2024-10-14 14:39:07 +0200

    configureplus status: print-out of global vs local variable values.

--- build/platforms/linux-gnu/.configureplus/global.mk
+++ build/platforms/linux-gnu/.configureplus/global.mk
@@ -1,5 +1,5 @@
 
-# Generated with configureplus (CONFIGUREPLUS_VERSION=0.14.2, CONFIGUREPLUS_PWD=/home/muenalan/git-workdirs/configureplus/build/platforms/linux-gnu)
+# Generated with configureplus (CONFIGUREPLUS_VERSION=0.14.5, CONFIGUREPLUS_PWD=/home/muenalan/git-workdirs/configureplus/build/platforms/linux-gnu)
 # Note: space before a comment are part of a variable
 
 CONFIGUREPLUS_SESSION=$(shell cat .configureplus/global/CONFIGUREPLUS_SESSION)
