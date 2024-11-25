38e7cc364505506c27103291ba29d04543da8dc8
Murat Uenalan <murat.uenalan@gmail.com>
2024-11-25 13:35:06 +0100

    minor: untracked dynamically created files

--- build/platforms/linux-gnu/.configureplus/global.mk
+++
@@ -1,5 +0,0 @@
-
-# Generated with configureplus (CONFIGUREPLUS_VERSION=0.16.2, CONFIGUREPLUS_PWD=/home/muenalan/git-workdirs/configureplus/build/platforms/linux-gnu)
-# Note: space before a comment are part of a variable
-
-CONFIGUREPLUS_SESSION=$(shell cat .configureplus/global/CONFIGUREPLUS/SESSION)
